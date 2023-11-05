9346678b69cb53832867c65aca817ac9497de21a
Vishal Panwar <119125256+vishallpanwar@users.noreply.github.com>
2023-11-05 23:10:56 +0530

    Add files via upload

---
+++ Operations/operations.js
@@ -0,0 +1,29 @@
+let val=3;
+let negVal=-val;
+console.log(negVal);
+
+
+
+// Operations
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+
+let str1="Vishal";
+let str2=" Panwar";
+let str3=(str1+str2);
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");    // Dono me output 12 12 hi ayega
+
+
+console.log("1"+2+2);  // Yha pr 122 ayega
+console.log(1+2+"2");  // jbki yha pr 32 ayega
+
+
+let counter=10;
+counter++;
+console.log(counter);  // 100 ayega
